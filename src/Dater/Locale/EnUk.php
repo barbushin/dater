0efea4dc9ccbf467baa0d0d8bc40a504473e188b
felixmaier1989 <felixmaier1989@gmail.com>
2016-12-22 09:51:48 +0700

    Fix typo

--- src/Dater/Locale/EnUk.php
+++ src/Dater/Locale/EnUk.php
@@ -5,7 +5,7 @@ use Dater\Dater;
 
 class EnUk extends \Dater\Locale {
 
-	protected static $months = array('January', 'February', 'March', 'April', 'May', 'June', 'Jule', 'August', 'September', 'October', 'November', 'December');
+	protected static $months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
 	protected static $weekDays = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 	protected static $weekDaysShort = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
 
